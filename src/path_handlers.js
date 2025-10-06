8bdfa251c64750e6e54d994a6f7bf638476f5539
Kacper Ferber <kacperosferb@gmail.com>
2025-10-06 07:04:34 +0000

    a

--- src/path_handlers.js
+++ src/path_handlers.js
@@ -1,5 +1,19 @@
 import { readFileSync } from "node:fs";
+        function show_image(src, width, height,alt) {
+            // Create a new image element
+            let img = document.createElement("img");
 
+            // Set the source, width, 
+            // height, and alt attributes
+            img.src = src;
+            img.width = width;
+            img.height = height;
+            img.alt = alt;
+
+            // Append the image element
+            // to the body of the document
+            document.body.appendChild(img);
+        }
 const index_html = readFileSync("static/index.html");
 
 const pathConfigs = [
@@ -15,8 +29,9 @@ const pathConfigs = [
     path: "/hello",
     allowed_methods: ["GET"],
     handler: (req, res) => {
-      res.writeHead(200, { "Content-Type": "text/plain" });
-      res.end("hello world!\n");
+      // res.writeHead(200, { "Content-Type": "text/plain" });
+      // res.end("hello world!\n");
+      show_image("/home/studenpdnt/first-project/public/12384883_su45220.jpg", 300, 300, "logo")
     },
   },
     {
